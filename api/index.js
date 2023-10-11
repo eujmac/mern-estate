1c116502d93aff62b0bfe460fc5b66b8075d660d
João <joaomarcosjmac@gmail.com>
2023-10-11 09:48:05 -0300

    Create a test pi route

--- api/index.js
+++ api/index.js
@@ -1,14 +1,18 @@
 import express from "express"
-import moongose from "module"
 import mongoose from "mongoose"
 import dotenv from "dotenv"
+import userRouter from "./routes/user.route.js"
 
 dotenv.config()
+const app = express()
+
 mongoose
   .connect(process.env.MONGO)
   .then(() => console.log("Connected to MongoDB"))
   .catch((err) => console.log(`Not connected to MongoDB: Erro: ${err}`))
-const app = express()
+
+app.use("/api/user", userRouter)
+
 app.listen(3000, () => {
   console.log("Server is running on port 3000!")
 })
