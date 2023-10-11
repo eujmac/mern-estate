bf2adb69f45e4ac2e5b02c290d2a71a7628671bd
João <joaomarcosjmac@gmail.com>
2023-10-11 12:55:41 -0300

    complete sign up page functionality

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,7 +1,13 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+import { defineConfig } from "vite"
+import react from "@vitejs/plugin-react-swc"
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  // criando um proxy do servidor front para o back
+  server: {
+    proxy: {
+      "/api": { target: "http://localhost:3000", secure: false },
+    },
+  },
   plugins: [react()],
 })
