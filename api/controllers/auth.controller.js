5ca0c983067cfe0e342a5de08d63fdd7670c933d
João <joaomarcosjmac@gmail.com>
2023-10-16 10:42:26 -0300

    fixing another error

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -1,4 +1,4 @@
-import User from "../models/User.model.js"
+import User from "../models/user.model.js"
 import bcryptjs from "bcryptjs"
 import { errorHandler } from "../utils/erro.js"
 import jwt from "jsonwebtoken"
