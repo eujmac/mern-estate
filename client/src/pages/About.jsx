af425fba109fb8559c229ee0035460f12ef101aa
João <joaomarcosjmac@gmail.com>
2023-10-16 09:50:46 -0300

    Complete about page

--- client/src/pages/About.jsx
+++ client/src/pages/About.jsx
@@ -1,7 +1,31 @@
 import React from "react"
 
 const About = () => {
-  return <div>About</div>
+  return (
+    <div className="py-20 p-x4 max-w-6xl mx-auto ">
+      <h1 className="text-3xl font-bold mb-4 text-slate-800">
+        About João Estate
+      </h1>
+      <p className="mb-4 text-slate-700">
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Assumenda
+        possimus rem magni perspiciatis saepe facere id vitae dolore deleniti
+        aliquid exercitationem reiciendis minima necessitatibus, praesentium,
+        incidunt ipsam commodi! Accusamus, rem!
+      </p>
+      <p className="mb-4 text-slate-700">
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Assumenda
+        possimus rem magni perspiciatis saepe facere id vitae dolore deleniti
+        aliquid exercitationem reiciendis minima necessitatibus, praesentium,
+        incidunt ipsam commodi! Accusamus, rem!
+      </p>
+      <p className="mb-4 text-slate-700">
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Assumenda
+        possimus rem magni perspiciatis saepe facere id vitae dolore deleniti
+        aliquid exercitationem reiciendis minima necessitatibus, praesentium,
+        incidunt ipsam commodi! Accusamus, rem!
+      </p>
+    </div>
+  )
 }
 
 export default About
